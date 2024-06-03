53b02e39157162e31f98e44a493915a3534dfe2e
Natalie <110455162+nkurtys@users.noreply.github.com>
2024-06-03 09:01:46 +0200

    add function file
    add function file with needed functions from MastodonScrape.py
    Adjust style.css for green layout and style
    clean up html files

--- WIP/app/routes.py
+++ WIP/app/routes.py
@@ -1,7 +1,6 @@
-from flask import render_template
+from flask import render_template, request
 import sqlite3
-from MastodonScrape import makeApp, searchPeriod
-from app import app
+from app import app, functions
 
 @app.route('/')
     
@@ -9,6 +8,7 @@ from app import app
 def index():
     user = {'username': 'Natalie'}
     return render_template('index.html', title='Home', user=user)
+
 @app.route('/database')
 def database():
     #Access Database
@@ -20,19 +20,28 @@ def database():
     #Detect Qfever in Content for highlighting
 
     return render_template('database.html', title='Database Lookup', posts=posts)
+
 @app.route('/search')
 def search():
     ## imprt funtion from main.py
+
+    query = request.args.get("query", "")
+    start_date = request.args.get("start_date", "")
+    end_date = request.args.get("end_date", "")
+    
+    if query:
+        result = functions.searchInstance(instance="mastodon.social", query=query, start_date=start_date, end_date=end_date)
+    else:
+        result = False
     # try: 
     #     statuses = searchPeriod("mastodon.social", )
-        return render_template('search.html', title='Search-Tool'
-                               #, statuses=statuses
-                               )
+    return render_template('search.html', title='Search-Tool', posts=result)
 
     #except:
         
 
 @app.route('/admin')
 def admin():
+    #setup database
     user = {'username': 'Natalie'}
     return render_template('admin.html', title='Admin', user=user)
